8438a1095f3d9895d1992f70720ddc1d088db067
cashless-om <124866481+cashless-om@users.noreply.github.com>
2023-04-06 14:16:35 +0530

    Add files via upload

--- ProjectTest1/ProjectTest1/ProjectTest1/Form3.cs
+++ ProjectTest1/ProjectTest1/ProjectTest1/Form3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace ProjectTest1
 {
     public partial class Form3 : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gurav\Documents\test.mdf;Integrated Security=True;Connect Timeout=30");
+
         public Form3()
         {
             InitializeComponent();
@@ -19,7 +22,54 @@ namespace ProjectTest1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int ct = 0;
+            con.Open();
 
+            string query = "SELECT Un FROM register WHERE Un = @username";
+
+            SqlCommand command = new SqlCommand(query, con);
+
+            command.Parameters.AddWithValue("@username", textBox1.Text);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                ct++;
+                reader.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username");
+                reader.Close();
+            }
+
+
+            string query2 = "SELECT recover FROM register WHERE recover = @recover";
+
+            SqlCommand command2 = new SqlCommand(query2, con);
+
+            command2.Parameters.AddWithValue("@recover", textBox2.Text);
+
+            SqlDataReader reader2 = command2.ExecuteReader();
+
+            if (reader2.HasRows)
+            {
+                ct++;
+                reader2.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid answer");
+                reader2.Close();
+
+            }
+            con.Close();
+            if (ct > 1)
+            {
+                Form4 f4 = new Form4();
+                f4.Show();
+            }
         }
     }
 }
